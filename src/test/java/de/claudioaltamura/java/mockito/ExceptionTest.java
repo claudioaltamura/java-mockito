700a6ed57db860e8fd2d5a7cbce40393bda392b9
Claudio Altamura <claudio@claudioaltamura.de>
2020-02-12 09:16:20 +0100

    chore: cleaning

--- src/test/java/de/claudioaltamura/java/mockito/ExceptionTest.java
+++ src/test/java/de/claudioaltamura/java/mockito/ExceptionTest.java
@@ -1,6 +1,5 @@
 package de.claudioaltamura.java.mockito;
 
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
