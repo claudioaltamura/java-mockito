3222ff3c0625e2ade48eafddc4e3c5993e0f53fc
claudioaltamura <claudio@claudioaltamura.de>
2020-09-28 14:38:07 +0200

    new: mock example

---
+++ src/test/java/de/claudioaltamura/java/mockito/WithOutMockitoAnnotations.java
@@ -0,0 +1,26 @@
+package de.claudioaltamura.java.mockito;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class WithOutMockitoAnnotations {
+
+  private Car car;
+
+  @BeforeEach
+  public void setUp() {
+    car = mock(Car.class);
+  }
+
+  @Test
+  void ifYouDontLikeAnnotations() {
+    when(car.getManuefactor()).thenReturn("Mercedes");
+
+    assertEquals("Mercedes", car.getManuefactor());
+  }
+
+}
